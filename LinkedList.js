2890549842e3a29f4d38454e523fb77f897c5989
Demetrios <jkopatsis@gmail.com>
2023-03-27 11:10:39 -0700

    General LL functions complete

--- LinkedList.js
+++ LinkedList.js
@@ -5,11 +5,11 @@ function Node(arg=null){
     }
 }
 
-function LinkedList(node){
+const LinkedList = function(node){
     
-    var head = node;
-
-    var append = function(node){
+    let head = node;
+    
+    const append = function(node){
         let current = head;
         while (current.next !== null){
             current = current.next;
@@ -17,7 +17,7 @@ function LinkedList(node){
         current.next = node;
     }
 
-    var size = () => {
+    const size = () => {
         let current = head;
         let count = 1;
         while (current.next !== null){
@@ -27,7 +27,7 @@ function LinkedList(node){
         return count;
     }
 
-    var tail = () => {
+    const tail = () => {
         let current = head;
         while(current.next !== null){
             current = current.next;
@@ -35,27 +35,78 @@ function LinkedList(node){
         return current;
     }
 
-    return {head, append, size, tail}
+    const at = function(prov){
+        let current = head;
+        let index = 0;
+        while (current.next !== null && index < prov){
+            current= current.next;
+            index++;
+        }
+        if (index == prov){
+            return current;
+        } else{
+            return "Not in list"
+        }
+    }
+
+    const pop = () => {
+        let current = head;
+        if (current.next !== null){
+            while (current.next.next !== null){
+                current = current.next;
+            }
+            current.next = null;
+        }
+    }
+
+    const toString = () => {
+        let str = "";
+        let current = head;
+        while (current.next !== null){
+            str += `( ${current.value} ) => `;
+            current = current.next;
+        }
+        str += `( ${current.value} )`
+        return str;
+    }
+
+    const find = function(value){
+        let current = head;
+        let index = 0;
+        while (current != null){
+            if(current.value === value){
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return null;
+    }
+
+    const contains = function(value){
+        let current = head;
+        while (current != null){
+            if(current.value === value){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    const prepend = function(node){
+        let current = head;
+        head = node;
+        node.next = current;
+    }
+
+
+    return {head, append, size, tail, at, pop, toString, find, contains, prepend}
 }
 
-
-let one = Node(1);
-let list = LinkedList(one);
-console.log(list.head);
-console.log(list.size());
-console.log(list.tail());
-console.log(" ");
-
-let two = Node(2);
-list.append(two);
-console.log(list.head);
-console.log(list.size());
-console.log(list.tail());
-console.log(" ");
-
-let three = Node(3);
-list.append(three);
-console.log(list.head);
-console.log(list.size());
-console.log(list.tail());
-console.log(" ");
+let list = LinkedList(Node(1));
+list.append(Node(2));
+list.append(Node(3));
+console.log(list.toString());
+list.prepend(Node(0));
+console.log(list.toString());
