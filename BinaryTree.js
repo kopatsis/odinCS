1122591ed9b21b763726e9108a23ded01863a28a
Demetrios <jkopatsis@gmail.com>
2023-03-28 13:33:48 -0700

    prettyPrint and functionality implemented

--- BinaryTree.js
+++ BinaryTree.js
@@ -6,9 +6,34 @@ function Node(arg=null){
     }
 }
 
-const BinaryTree = (node=null) => {
+const Tree = (node=null) => {
 
     let root = node;
 
+    return {root}
 
 }
+
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null) {
+       return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+}
+
+let tree = Tree(Node(0));
+console.log(tree.root);
+tree.root.left = Node(1);
+tree.root.right = Node(2);
+tree.root.left.left = Node(3);
+tree.root.left.right = Node(4);
+tree.root.right.left = Node(5);
+tree.root.right.right = Node(6);
+prettyPrint(tree.root);
