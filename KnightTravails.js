195d7265054aa17dbb4e8e3d880db545d703cb82
Demetrios <jkopatsis@gmail.com>
2023-03-29 13:59:16 -0700

    BFS shortest path alogorithm success

--- KnightTravails.js
+++ KnightTravails.js
@@ -35,39 +35,92 @@ const Graphing = () => {
         return graph;
     }
 
-    return {generateKeys, retGraph, generatePairs}
+    const fastestPath = (start, end) => {
+
+        if (start[0] < 0 || start[0] > 7 || start[1] < 0 || start[1] > 7){
+            return "Not on board"
+        }
+        if (end[0] < 0 || end[0] > 7 || end[1] < 0 || end[1] > 7){
+            return "Not on board"
+        }
+
+        const st = start[0]*8 + start[1];
+        const en = end[0]*8 + end[1];
+
+        let main = new Array(64).fill(null);
+        let visited = new Set();
+        let queue = new Queue();
+
+        main[st] = [st];
+        queue.enqueue(st);
+
+        while (!queue.emp){
+            let curr = queue.dequeue();
+            for (const pt of graph[curr]){
+                if (!visited.has(pt)){
+                    queue.enqueue(pt);
+                }
+                if (main[pt]===null || main[curr].length+1 < main[pt].length){
+                    main[pt] = main[curr].map((x) => x);
+                    main[pt].push(pt);
+                }
+            }
+            visited.add(curr);
+        }
+
+        let endarr = main[en];
+        let ret = [];
+        for (const lin of endarr){
+            ret.push([Math.floor(lin/8), lin%8]);
+        }
+        return ret;
+
+    }
+
+    const connections = (point) => {
+        for (const pt of graph[point[0]*8 + point[1]]){
+            console.log(pt);
+        }
+    }
+
+    return {generateKeys, retGraph, generatePairs, fastestPath, connections}
 }
 
 
 class Queue {
     constructor() {
-        this.items = {}
-        this.frontIndex = 0
-        this.backIndex = 0
+      this.elements = {};
+      this.head = 0;
+      this.tail = 0;
     }
-    enqueue(item) {
-        this.items[this.backIndex] = item
-        this.backIndex++
-        return item + ' inserted'
+    enqueue(element) {
+      this.elements[this.tail] = element;
+      this.tail++;
     }
     dequeue() {
-        const item = this.items[this.frontIndex]
-        delete this.items[this.frontIndex]
-        this.frontIndex++
-        return item
+      const item = this.elements[this.head];
+      delete this.elements[this.head];
+      this.head++;
+      return item;
     }
     peek() {
-        return this.items[this.frontIndex]
+      return this.elements[this.head];
     }
-    get printQueue() {
-        return this.items;
+    get len() {
+      return this.tail - this.head;
     }
-}
+    get emp() {
+      return this.len === 0;
+    }
+  }
 
-// let board = Graphing();
-// board.generateKeys();
-// board.generatePairs();
-// console.log(board.retGraph());
+let board = Graphing();
+board.generateKeys();
+board.generatePairs();
+console.log(board.retGraph());
+console.log(board.fastestPath([0,6],[4,1]));
+// board.connections([4,1]);
+
 
 
 // let board = [];
@@ -80,11 +133,22 @@ class Queue {
 // }
 // console.log(board);
 
-let rod = new Queue();
-rod.enqueue(2);
-rod.enqueue(3);
-rod.enqueue(4);
-rod.enqueue(5);
-console.log(rod.dequeue());
-console.log(rod.dequeue());
-console.log(rod.dequeue());
+// console.log(new Array(64).fill(null)[35]);
+
+// let rod = new Queue();
+// console.log(rod.emp);
+// rod.enqueue(4);
+// rod.enqueue(3);
+// rod.enqueue(2);
+// rod.enqueue(1);
+// console.log(rod.emp);
+// console.log(rod.len);
+// rod.dequeue();
+// rod.dequeue();
+// console.log(rod.emp);
+// console.log(rod.len);
+// rod.dequeue();
+// rod.dequeue();
+// console.log(rod.emp);
+// console.log(rod.len);
+
