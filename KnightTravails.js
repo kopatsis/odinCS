da458130cec346db85bed792041599764024d657
Demetrios <jkopatsis@gmail.com>
2023-03-29 13:24:19 -0700

    queue implemented

--- KnightTravails.js
+++ KnightTravails.js
@@ -38,10 +38,36 @@ const Graphing = () => {
     return {generateKeys, retGraph, generatePairs}
 }
 
-let board = Graphing();
-board.generateKeys();
-board.generatePairs();
-console.log(board.retGraph());
+
+class Queue {
+    constructor() {
+        this.items = {}
+        this.frontIndex = 0
+        this.backIndex = 0
+    }
+    enqueue(item) {
+        this.items[this.backIndex] = item
+        this.backIndex++
+        return item + ' inserted'
+    }
+    dequeue() {
+        const item = this.items[this.frontIndex]
+        delete this.items[this.frontIndex]
+        this.frontIndex++
+        return item
+    }
+    peek() {
+        return this.items[this.frontIndex]
+    }
+    get printQueue() {
+        return this.items;
+    }
+}
+
+// let board = Graphing();
+// board.generateKeys();
+// board.generatePairs();
+// console.log(board.retGraph());
 
 
 // let board = [];
@@ -53,3 +79,12 @@ console.log(board.retGraph());
 //     board.push(row);
 // }
 // console.log(board);
+
+let rod = new Queue();
+rod.enqueue(2);
+rod.enqueue(3);
+rod.enqueue(4);
+rod.enqueue(5);
+console.log(rod.dequeue());
+console.log(rod.dequeue());
+console.log(rod.dequeue());
